32fbf7b8f41e758bccbc989388896432fe7a5140
Drew Conly <atconly@gmail.com>
2016-07-06 11:43:21 -0600

    test new bash script

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var startTime  = new Date();
 btn.onclick = function() {
     modal.style.display = "block";
     var lapsedTime = new Date() - startTime;
-    document.getElementById("lapsedTime").innerHTML = "Minutes on site: " + (lapsedTime / 6000);
+    document.getElementById("lapsedTime").innerHTML = "Minutes on site: " + (lapsedTime * 1000 / 60).toFixed(2);
 }
 
 span.onclick = function() {
@@ -28,3 +28,35 @@ d3.select("body").transition()
   .duration(8000)
   .style("background-color", "black")
   .style("color", "rgb(255, 255, 255)");
+
+// Bubble Chart //
+
+document.onReady(function () {
+  var bubbleChart = new d3.svg.BubbleChart({
+    supportResponsive: true,
+    //container: => use @default
+    size: 600,
+    //viewBoxSize: => use @default
+    innerRadius: 600 / 3.5,
+    //outerRadius: => use @default
+    radiusMin: 50,
+    //radiusMax: use @default
+    //intersectDelta: use @default
+    //intersectInc: use @default
+    //circleColor: use @default
+    data: {
+      items: [
+        {text: "Java", count: "236"},
+        {text: ".Net", count: "382"},
+        {text: "Php", count: "170"},
+        {text: "Ruby", count: "123"},
+        {text: "D", count: "12"},
+        {text: "Python", count: "170"},
+        {text: "C/C++", count: "382"},
+        {text: "Pascal", count: "10"},
+        {text: "Something", count: "170"},
+      ],
+      eval: function (item) {return item.count;},
+      classed: function (item) {return item.text.split(" ").join("");}
+    },
+  )};
