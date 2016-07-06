9213255460458f2cb733a3ef669ee5c6fe34cf0c
Drew Conly <atconly@gmail.com>
2016-07-05 20:25:00 -0600

    add gratuitous d3

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ console.log('here... we... go');
 d3.selectAll(".ipsum").style("color", function(d, i) {
   return i % 2 ? "#00A0E9" : "#BE0081";
 });
+
+d3.select("body").transition()
+  .duration(8000)
+  .style("background-color", "black")
+  .style("color", "rgb(255, 255, 255)");
