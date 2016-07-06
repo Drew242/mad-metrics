12afcaff0b1442e36ee88c5b86d84d8d4daf20a7
Drew Conly <atconly@gmail.com>
2016-07-05 21:48:07 -0600

    create modal for graph(s)

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-console.log('here... we... go');
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("metric-madness");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 d3.selectAll(".ipsum").style("color", function(d, i) {
   return i % 2 ? "#00A0E9" : "#BE0081";
