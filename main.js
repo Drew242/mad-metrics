1874664cd165eded25513a801bc702439c1a7e8a
Drew Conly <atconly@gmail.com>
2016-07-06 14:54:23 -0600

    make timeSum function work

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-window.onload    = timer;
-var modal        = document.getElementById('myModal');
-var btn          = document.getElementById("metric-madness");
-var span         = document.getElementsByClassName("close")[0];
+window.onload     = timer;
+var modal         = document.getElementById('myModal');
+var btn           = document.getElementById("metric-madness");
+var span          = document.getElementsByClassName("close")[0];
+var divTime       = document.getElementsByTagName("div");
+var welcomeDiv    = [];
+var aboutDiv      = [];
+var historyDiv    = [];
+var studentDiv    = [];
+var dayDiv        = [];
+var communityDiv  = [];
+var boulderDiv    = [];
+var resourcesDiv  = [];
+var additionalDiv = [];
+var signupDiv     = [];
+
+for (i = 0; i < divTime.length; i++) {
+  divTime[i].addEventListener("mouseenter", function() {
+    startTime = totalSeconds;
+    return startTime;
+  })
+}
+
+for (i = 0; i < divTime.length; i++) {
+  divTime[i].addEventListener("mouseleave", function() {
+    var endTime  = totalSeconds;
+    hoverTime    = (endTime - startTime);
+    var location = this.id + "Div";
+    aboutDiv.push(hoverTime);
+  })
+}
 
 function timer() {
   var interval = setInterval(setTime, 1000);
@@ -11,13 +38,23 @@ function timer() {
       }
 }
 
+function timeSum(divName) {
+  totalDivSeconds = 0;
+  for (i = 0; i < divName.length; i++) {
+    totalDivSeconds += divName[i];
+  }
+  return totalDivSeconds;
+}
+
 btn.onclick = function() {
     modal.style.display = "block";
     document.getElementById("timeOnSite").innerHTML = "Seconds on site: " + (totalSeconds);
     document.getElementById("distanceScrolled").innerHTML = "Distance Scrolled: " + 42 + "px";
     document.getElementById("percOfPage").innerHTML = "Percentage of Page Scrolled: " + (42 / 6) + "%";
     document.getElementById("timeB4SignUp").innerHTML = "Time before clicking signup: " + (totalSeconds / 7).toFixed(2) + " seconds";
+    document.getElementById("aboutDiv").innerHTML = "Seconds spent on About: " + timeSum(aboutDiv);
 }
+
 
 span.onclick = function() {
     modal.style.display = "none";
